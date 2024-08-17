baf2489c5146ba9f89cace0af0e5024ec5e72d53
Gio <gio.devera@ucalgary.ca>
2024-08-17 16:31:18 -0600

    Editpage has flip-answer/multichoice button, shows number of questions set

--- quiz-gameshow/src/frontend/Editpage.js
+++ quiz-gameshow/src/frontend/Editpage.js
@@ -1,5 +1,6 @@
-import {Container, FormGroup, Slider, Typography, Checkbox, FormControlLabel} from '@mui/material';
-import { red, blue } from '@mui/material/colors';
+import {Container, FormGroup, Slider, Checkbox, FormControlLabel, Button, ToggleButton, ToggleButtonGroup, TextField, SpeedDial, SpeedDialIcon, SpeedDialAction} from '@mui/material';
+import {blue } from '@mui/material/colors';
+import { useState } from 'react';
 import '../css/Editpage.css'
 
 const categoryMarks = [
@@ -24,7 +25,37 @@ const questionMarks = [
     }
 ]
 
+const tempCategories = [
+    {name: 'Space', icon: '1'},
+    {name: 'History', icon: '2'},
+    {name: 'Misc', icon: '3'},
+    {name: 'Psyche', icon: '4'}
+]
+
 export default function Editpage(){
+
+    const [categoryNum, setCategoryNum] = useState(5);
+    const [questionNum, setQuestionNum] = useState(5);
+    const [questionType, setQuestionType] = useState("multiple-choice");
+
+    const handleCategoryChange = (event, newNumber) => {
+        setCategoryNum(newNumber);
+    }
+
+    const handleQuestionNumChange = (event, newQuestionNumber) => {
+        setQuestionNum(newQuestionNumber);
+    }
+
+    const handleQuestionTypeChange = (event, newQuestionType) =>{
+        setQuestionType(newQuestionType);
+    }
+
+    const shoutout = () => {
+        console.log("The number of categories is " + categoryNum);
+        console.log("The number of questions is: " + questionNum);
+        console.log("The type of question is: " + questionType);
+    }
+
     return(
         <>
             <div id="editBackground">
@@ -34,9 +65,10 @@ export default function Editpage(){
                             Number of Categories
                         </p>
                         <Slider 
-                            defaultValue={5}
+                            value={categoryNum}
                             aria-label='Temperature'
                             valueLabelDisplay='on'
+                            onChange={handleCategoryChange}
                             step={1}
                             marks = {categoryMarks}
                             min={2}
@@ -50,9 +82,10 @@ export default function Editpage(){
                             Number of Questions per Category
                         </p>
                         <Slider
-                            defaultValue={5}
+                            value={questionNum}
                             aria-label='Temperature'
                             valueLabelDisplay='on'
+                            onChange={handleQuestionNumChange}
                             step={1}
                             marks = {questionMarks}
                             min={3}
@@ -64,17 +97,85 @@ export default function Editpage(){
                         sx={{color: blue[800]}}
                         class="selectLabels"
                         label="Daily Doubles?"/>
+
                         <FormControlLabel 
                         control={<Checkbox/>} 
                         sx={{color: blue[800]}}
                         class="selectLabels"
                         label="Final Jeoprady?"/>
+
+                        <ToggleButtonGroup
+                            exclusive
+                            value={questionType}
+                            onChange={handleQuestionTypeChange}
+                            >
+                                <ToggleButton value="multiple-choice">
+                                    Multiple Choice
+                                </ToggleButton>
+                                <ToggleButton value="flip-answer">
+                                    Flip Answer
+                                </ToggleButton>
+                        </ToggleButtonGroup>
                     </FormGroup>
+                    <Button
+                        onClick={shoutout}
+                    >
+                        HERE
+                    </Button>
                 </Container>
                 <Container id="textBoxes" class="editBoxes">
-                    <Typography>
-                        THERE WILL BE BOXES TO INPUT FOR YOUR ANSWERS 
-                    </Typography>
+                    {questionType === "flip-answer" ? (
+                        <>
+                        <SpeedDial
+                            style={{position:'absolute', top:50, right:520}}
+                            direction='down'
+                            icon={<SpeedDialIcon/>}
+                            ariaLabel="Example"
+                        >
+                            {tempCategories.map((category) => (
+                                <SpeedDialAction
+                                    key={category.name}
+                                    icon={category.icon}
+                                    tooltipTitle={category.name}
+                                />
+                            ))}
+
+                        </SpeedDial>
+                        {Array.from({length: questionNum}, (_, index) =>(
+                            <div 
+                                style={{
+                                    display:'flex',
+                                    width:'500px',
+                                }}
+                            >
+                                <TextField
+                                    key={index}
+                                    className='flipBoxes'
+                                    label="Enter Question"
+                                    style={{
+                                        marginRight:'10px',
+                                        marginBottom:'10px',
+                                    }}
+                                    multiline
+                                >
+                                </TextField>
+                                <TextField
+                                    label="Enter Answer"
+                                    className='flipBoxes'
+                                    multiline
+                                >
+                                </TextField>
+                            </div>
+                        ))}
+                    </>
+                        )
+                        :
+                        (
+                                <>
+                                    Yeah idk
+                                </>
+                        )
+                    }
                 </Container>
             </div>
         </>
